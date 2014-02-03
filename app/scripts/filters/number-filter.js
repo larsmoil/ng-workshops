d11497e56f8e66b03471505d7338cc574a5c41ac
Lars-Olof Moilanen <lars.olof.moilanen@gmail.com>
2014-02-03 16:16:43 +0100

    Simple implementation (using if/while/for)

--- app/scripts/filters/number-filter.js
+++ app/scripts/filters/number-filter.js
@@ -3,6 +3,22 @@
 angular.module('ngWorkshopsApp.filters')
   .filter('number', function () {
     return function (input) {
-      return input;
+      var chars = (input || '').split('').reverse();
+      var tmp = [];
+
+      while (chars.length) {
+        for (var i = 0; i < 3; i += 1) {
+          if (chars.length) {
+            tmp.push(chars.shift());
+          }
+        }
+        tmp.push(' ');
+      }
+
+      if (tmp[tmp.length - 1] === ' ') {
+        tmp.pop();
+      }
+
+      return tmp.reverse().join('');
     };
   });
